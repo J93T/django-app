6eb1feca6c66faf2b90a8dd0f97a79c0c27037c5
Jeremy Tschirner <jeretschirner@web.de>
2018-09-10 22:24:10 +0200

    repaired .gitignore, added class based view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.views.generic import ListView
 from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
 
 def home(request):
     context = {
